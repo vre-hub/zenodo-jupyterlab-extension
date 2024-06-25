fc7cb1783621f90e5db54cf323750d67468b94e0
mrzengel <michael.ryan.zengel@gmail.com>
2024-06-25 11:16:02 +0200

    developed test eossr script

---
+++ zenodo_test/eossr_test.py
@@ -0,0 +1,12 @@
+#from eossr.api.zenodo.zenodo import _query as query
+from eossr.api.zenodo.zenodo import _search as query
+
+response = query('records', '', communities='ESCAPE2020')
+
+count = 0
+
+for record in response:
+    count+=1
+    print('Title: ' + record['metadata']['title'] + ', Date Published: ' + record['metadata']['publication_date'])
+
+print(count)
