95c7fe22d4c106a5803875385969ca6815d663e7
mrzengel <michael.ryan.zengel@gmail.com>
2024-10-20 15:17:44 -0500

    testing fix

--- zenodo_jupyterlab/server/handlers.py
+++ zenodo_jupyterlab/server/handlers.py
@@ -68,15 +68,19 @@ class FileBrowserHandler(APIHandler):
     async def get(self):
         # Use the home directory as the root directory
         #root_dir = os.getenv("HOME")
-        """relative_path = self.get_query_argument('path', '')
-        full_path = os.path.join(os.getcwd(), relative_path) """
-        full_path = os.getcwd()
+        relative_path = self.get_query_argument('path', '')
+        full_path = os.path.join(os.getcwd(), relative_path)
 
-        # Check if the directory exists
-        if not os.path.isdir(full_path):
+        if '..' in full_path or not os.path.isdir(full_path):
             self.set_status(404)
             self.finish({"error": "Directory not found"})
             return
+
+        """ # Check if the directory exists
+        if not os.path.isdir(full_path):
+            self.set_status(404)
+            self.finish({"error": "Directory not found"})
+            return """
 
         entries = []
         for entry in os.listdir(full_path):
@@ -87,7 +91,8 @@ class FileBrowserHandler(APIHandler):
             entries.append({
                 "name": entry,
                 "type": "directory" if os.path.isdir(entry_path) else "file",
-                "path": os.path.relpath(entry_path, os.getcwd()).replace('\\', '/'),  # Use relative path from home directory
+                #"path": os.path.relpath(entry_path, os.getcwd()).replace('\\', '/'),  # Use relative path from home directory,
+                "path": os.path.join(relative_path, entry),
                 "modified": datetime.fromtimestamp(entry_stat.st_mtime, tz=timezone.utc).isoformat(),
                 "size": entry_stat.st_size
             })
